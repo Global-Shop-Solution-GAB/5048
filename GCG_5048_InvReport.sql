7f6f4ca2ea2c43c624fad991d0b0e86322a6945b
Service Web <sw@gssmail.com>
2024-03-13 11:03:45 -0500

    Commit changes in C:\Websites\Git\emarlow\5048\GCG_5048_InvReport.sql

---
+++ GCG_5048_InvReport.sql
@@ -0,0 +1,12 @@
+/* ==========================================
+ * TABLE: GCG_5048_InvReport
+ * ========================================== */
+CREATE TABLE "GCG_5048_InvReport"(
+ "PART" CHAR(20),
+ "PL" CHAR(2),
+ "DESCRIPTION" CHAR(30),
+ "QTY_AVAIL" NUMERIC(12,4),
+ "QTY_ONHAND" NUMERIC(12,4),
+ "QTY_REQUIRED" NUMERIC(12,4));
+
+
